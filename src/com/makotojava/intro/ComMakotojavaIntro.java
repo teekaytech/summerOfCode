587c75779dd237a49736f8c1daec5f54132d5365
teekaytech <tolalere439@stu.ui.edu.ng>
2018-09-16 17:46:36 +0100

    Inittial files

--- src/com/makotojava/intro/ComMakotojavaIntro.java
+++ src/com/makotojava/intro/ComMakotojavaIntro.java
@@ -18,17 +18,17 @@ public class ComMakotojavaIntro {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        String input = "Here is a WikiWord followed by AnotherWikiWord, then SomeWikiWord.";
-        Pattern pattern = Pattern.compile("[A-Z][a-z]*([A-Z][a-z]*)+");
-        Matcher matcher = pattern.matcher(input);
+//    public static void main(String[] args) {
+//        String input = "Here is a WikiWord followed by AnotherWikiWord, then SomeWikiWord.";
+//        Pattern pattern = Pattern.compile("[A-Z][a-z]*([A-Z][a-z]*)+");
+//        Matcher matcher = pattern.matcher(input);
 //        Logger.getAnonymousLogger().info("Before: " + input);
 //        String result = matcher.replaceAll("replacement");
 //        String result = matcher.replaceFirst("replacement");
 //        Logger.getAnonymousLogger().info("After: " + result);
-        while (matcher.find()) {
-            Logger.getAnonymousLogger().info("Found this wiki word: " + matcher.group());
-        }
-    }
-    
+//        while (matcher.find()) {
+//            Logger.getAnonymousLogger().info("Found this wiki word: " + matcher.group());
+//        }
+//    }
+//    
 }
